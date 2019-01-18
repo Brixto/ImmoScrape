91cc5b1ab61381a5b9618032e85fe9450a7d248e
Volkan Altundal <volkan@Volkans-MBP.fritz.box>
2019-01-18 10:31:33 +0100

    moved functions around

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
 var express = require('express');
-const puppeteer = require('puppeteer')
+const scrape = require('./scrape')
 var app = express();
 
 app.get('/quoka', function (req, res) {
-    loadQuokaList()
+    scrape.loadQuokaList()
         .then(x => {
-            res.send(x);
+            res.send(JSON.stringify(x));
         })
 });
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('Erfolgreich auf Port 3000 gestartet.');
 });
-
-async function loadQuokaList() {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.goto('https://www.quoka.de/')
-
-    await page.evaluate(() => {
-        document.querySelector('#search1').value = '3 Zimmerwohnung'
-        document.querySelector('#city').value = 'Coburg'
-        document.querySelector('#searchbutton').click()
-    })
-
-    await page.waitForNavigation()
-
-    const result = await page.evaluate(() =>
-        Array.from(document.querySelectorAll('div.q-col div.description'))
-            .map(x => x.innerHTML.trim()))
-
-    await browser.close()
-
-    return result;
-}
--- scrape.js
+++ scrape.js
@@ -1,23 +1,25 @@
 const puppeteer = require('puppeteer')
 
-export async function loadQuokaList() {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto('https://www.quoka.de/')
+module.exports = {
+  loadQuokaList: async function () {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto('https://www.quoka.de/')
 
-  await page.evaluate(() => {
-    document.querySelector('#search1').value = '3 Zimmerwohnung'
-    document.querySelector('#city').value = 'Coburg'
-    document.querySelector('#searchbutton').click()
-  })
+    await page.evaluate(() => {
+      document.querySelector('#search1').value = '3 Zimmerwohnung'
+      document.querySelector('#city').value = 'Coburg'
+      document.querySelector('#searchbutton').click()
+    })
 
-  await page.waitForNavigation()
+    await page.waitForNavigation()
 
-  const result = await page.evaluate(() =>
-    Array.from(document.querySelectorAll('div.q-col div.description'))
-      .map(x => x.innerHTML.trim()))
+    const result = await page.evaluate(() =>
+      Array.from(document.querySelectorAll('div.q-col div.description'))
+        .map(x => x.innerHTML.trim()))
 
-  await browser.close()
+    await browser.close()
 
-  return result;
+    return result;
+  }
 }
