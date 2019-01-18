1f44b05230a71b1328ce4119eedc5c04709d015e
Volkan Altundal <volkan@Volkans-MBP.fritz.box>
2019-01-18 11:25:33 +0100

    added cors

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-var express = require('express');
+var express = require('express')
 const scrape = require('./scrape')
-var app = express();
+var cors = require('cors')
+
+var app = express()
+
+app.use(cors())
 
 app.get('/quoka', function (req, res) {
     scrape.loadQuokaList()
         .then(x => {
-            res.send(JSON.stringify(x));
+            res.send(JSON.stringify(x))
         })
-});
+})
 
 app.listen(3000, function () {
-    console.log('Erfolgreich auf Port 3000 gestartet.');
-});
+    console.log('Erfolgreich auf Port 3000 gestartet.')
+})
